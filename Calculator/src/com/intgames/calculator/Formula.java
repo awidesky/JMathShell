57482dc010f85766e0396ffecef6030e6003ec09
awidesky <awidesky@naver.com>
2020-07-14 19:43:01 +0900

    add BigMath.java

--- Calculator/src/com/intgames/calculator/Formula.java
+++ Calculator/src/com/intgames/calculator/Formula.java
@@ -20,12 +20,12 @@ import java.util.Map;
  */
 public class Formula {
 
-	/** Formula parsed to <code>List<Token></code> */
+	/** Formula parsed to <code>List<com.intgames.calculator.Token></code> */
 	private List<Token> tokens;
 	/** Custom functions */
 	private Map<String, Function> customFunc;
 	/** Custom Variables */
-	private Map<String, Token> customVariable;
+	private Map<String, Token> customVariables;
 	
 	/** Standard functions */
 	private static final Map<String, Function> basicFunc = new HashMap<>();
@@ -36,7 +36,7 @@ public class Formula {
 		
 	}
 	/** Standard variables */
-	private static final Map<String, Token> basicVariable = new HashMap<>();
+	private static final Map<String, Token> basicVariables = new HashMap<>();
 	static {
 		
 		//TODO : add basic variables
@@ -47,47 +47,3 @@ public class Formula {
 	
 	
 }// Formula class end
-
-/** Object represents each monomials of <code>Formula</code> */
-class Token {
-	
-	public static final Token PLUS = new Token("+", TokenType.OPERATOR);
-	public static final Token MINUS = new Token("-", TokenType.OPERATOR);
-	public static final Token MULTIPLY = new Token("*", TokenType.OPERATOR);
-	public static final Token DIVISION = new Token("/", TokenType.OPERATOR);
-	public static final Token BRACKET_OPEN = new Token("(", TokenType.OPERATOR);
-	public static final Token BRACKET_CLOSE = new Token(")", TokenType.OPERATOR);
-	
-	/** Type of the token */
-	private TokenType type;
-	private BigDecimal num;
-	
-	enum TokenType {	NUMBER,		OPERATOR,		FUNCTION	}
-	
-	private Token(String string, TokenType type) {
-		
-		this.type = type;
-		
-		switch(type) {
-		
-		case NUMBER:
-			
-			num = new BigDecimal(string);
-			break;
-			
-		case FUNCTION:
-		
-			
-			
-		case OPERATOR:
-			
-		}
-	}
-	
-	
-	
-	
-}// Token class end
-
-/** Object represents functions of <code>Formula</code> */
-class Function{}
