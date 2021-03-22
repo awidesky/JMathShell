efbaa1fbca92b027fb05e7c988e63d3eb3511a8a
Eugene Hong <awidesky@naver.com>
2021-03-22 14:46:24 +0900

    Suppress Warning in LegacyFormula.java

--- src/com/intgames/calculator/LegacyFormula.java
+++ src/com/intgames/calculator/LegacyFormula.java
@@ -18,16 +18,20 @@ import java.util.Map;
  * 
  * 
  */
-public class Formula {
+public class LegacyFormula {
 
 	/** Formula parsed to <code>List<Token></code> */
+	@SuppressWarnings("unused")
 	private List<Token> tokens;
 	/** Custom functions */
+	@SuppressWarnings("unused")
 	private Map<String, Function> customFunc;
 	/** Custom Variables */
+	@SuppressWarnings("unused")
 	private Map<String, Token> customVariable;
 	
 	/** Standard functions */
+	@SuppressWarnings("unused")
 	private static final Map<String, Function> basicFunc = new HashMap<>();
 	static {
 		
@@ -36,6 +40,7 @@ public class Formula {
 		
 	}
 	/** Standard variables */
+	@SuppressWarnings("unused")
 	private static final Map<String, Token> basicVariable = new HashMap<>();
 	static {
 		
@@ -49,22 +54,24 @@ public class Formula {
 }// Formula class end
 
 /** Object represents each monomials of <code>Formula</code> */
-class Token {
+class LegacyToken {
 	
-	public static final Token PLUS = new Token("+", TokenType.OPERATOR);
-	public static final Token MINUS = new Token("-", TokenType.OPERATOR);
-	public static final Token MULTIPLY = new Token("*", TokenType.OPERATOR);
-	public static final Token DIVISION = new Token("/", TokenType.OPERATOR);
-	public static final Token BRACKET_OPEN = new Token("(", TokenType.OPERATOR);
-	public static final Token BRACKET_CLOSE = new Token(")", TokenType.OPERATOR);
+	public static final LegacyToken PLUS = new LegacyToken("+", TokenType.OPERATOR);
+	public static final LegacyToken MINUS = new LegacyToken("-", TokenType.OPERATOR);
+	public static final LegacyToken MULTIPLY = new LegacyToken("*", TokenType.OPERATOR);
+	public static final LegacyToken DIVISION = new LegacyToken("/", TokenType.OPERATOR);
+	public static final LegacyToken BRACKET_OPEN = new LegacyToken("(", TokenType.OPERATOR);
+	public static final LegacyToken BRACKET_CLOSE = new LegacyToken(")", TokenType.OPERATOR);
 	
 	/** Type of the token */
+	@SuppressWarnings("unused")
 	private TokenType type;
+	@SuppressWarnings("unused")
 	private BigDecimal num;
 	
 	enum TokenType {	NUMBER,		OPERATOR,		FUNCTION	}
 	
-	private Token(String string, TokenType type) {
+	private LegacyToken(String string, TokenType type) {
 		
 		this.type = type;
 		
@@ -90,4 +97,4 @@ class Token {
 }// Token class end
 
 /** Object represents functions of <code>Formula</code> */
-class Function{}
+class LegacyFunction{}
